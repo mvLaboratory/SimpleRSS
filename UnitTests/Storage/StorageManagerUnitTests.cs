f2f3d6d39b88b90fbee129462458fcfd25ef5d14
Volodymyr Mykytiuk <mvlabaratory@gmail.com>
2017-12-05 00:52:46 +0200

    Storage managet test start

---
+++ UnitTests/Storage/StorageManagerUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Storage
+{
+    [TestClass]
+    public class StorageManagerUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
